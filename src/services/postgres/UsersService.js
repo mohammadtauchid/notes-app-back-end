d664bc56ce7ee1a05d8f5aeb67c3db0c31fc88c1
Tauchid <muhammadtauchid@gmail.com>
2023-03-24 19:41:58 +0700

    add salt

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -15,7 +15,7 @@ class UsersService {
     await this.verifyUserName(username);
 
     const id = `user-${nanoid(16)}`;
-    const hashedPassword = await bcrypt.hash(password);
+    const hashedPassword = await bcrypt.hash(password, 10);
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
       values: [id, username, hashedPassword, fullname],
