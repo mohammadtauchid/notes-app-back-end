fbaa76438f0225e0b88eac8356cf521a472496ca
Tauchid <muhammadtauchid@gmail.com>
2023-04-09 12:04:53 +0700

    collaboration implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -57,6 +57,16 @@ class UsersService {
       throw new InvariantError('Gagal menambahkan user baru. Username sudah digunakan');
     }
   }
+
+  async getUsersByUsername(username) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
+      values: [`%${username}%`],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
 }
 
 module.exports = UsersService;
